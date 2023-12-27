b44a1dc150aa476653579782dd26ac9c79ca6007
ragilputra25 <gel.ragel25@gmail.com>
2023-12-27 15:42:47 +0700

    tugas satu

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'siobat';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+
+?>
